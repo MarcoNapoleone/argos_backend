1958ea879e0d36858e82d3c3b9739898af459ff5
MarcoNapoleone <napoleonemarco25@gmail.com>
2022-10-18 13:36:47 +0200

    v. 0.1.7

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -6,18 +6,6 @@ const helper = {
             return {};
         } else if (rows?.length === 0) {
             return {}
-        } else if (rows?.length === 1){
-            return rows[0]
-        }
-        return rows;
-    },
-    emptyOrRow(rows: any) {
-        if (!rows) {
-            return {};
-        } else if (rows?.length === 0) {
-            return {}
-        } else if (rows?.length === 1){
-            return rows[0]
         }
         return rows;
     }
