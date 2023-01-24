a7e83bf22de5bf466d85f71ec57bd5b19ee003dd
MarcoNapoleone <napoleonemarco25@gmail.com>
2023-01-24 11:05:02 +0100

    v. 0.1.17 - object handler and folder adjustment

--- src/handlers/http/validationMessage.ts
+++ src/handlers/http/validationMessage.ts
@@ -3,9 +3,8 @@ import {Request} from "express";
 
 
 export const validationMessage = (req: Request): string => {
-  return '['
-    + validationResult(req).array()[0].msg
-    + '] at '
+  return validationResult(req).array()[0].msg
+    + ' at '
     + validationResult(req)
       .array()[0]
       .param
