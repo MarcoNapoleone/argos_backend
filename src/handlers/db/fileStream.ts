9be39fd27b82262eb0399456f506e043049fdbae
MarcoNapoleone <napoleonemarco25@gmail.com>
2023-03-06 13:57:40 +0100

    v. 0.3.2 - timetables

--- src/handlers/db/fileStream.ts
+++ src/handlers/db/fileStream.ts
@@ -1,1 +1,53 @@
-import {Storage} from "@google-cloud/storage";import {Folder} from "../../types/Folder";import {Express} from "express";import {format} from "util";const storage = new Storage({  projectId: process.env.GCLOUD_PROJECT_ID,  credentials: {    client_email: process.env.GCLOUD_CLIENT_EMAIL,    private_key: process.env.GCLOUD_PRIVATE_KEY.replace(/\\n/g, '\n')  }});const bucket = storage.bucket(process.env.GCLOUD_BUCKET_NAME);// get temporary link for file download from google cloud storage bucket (expires in 5 minutes)export async function getTemporaryLink(path: string, expires: number = 300000) {  const blob = bucket.file(path);  const data = await blob.getSignedUrl({    action: 'read',    expires: new Date(Date.now() + expires)  });  return data[0];}export async function upload(folder: Folder, name: string, file: Express.Multer.File): Promise<{ path: string }> {  return new Promise((resolve, reject) => {    const blob = bucket.file(`${folder}/${name.replace(/ /g, "_")}`)    const blobStream = blob.createWriteStream({      resumable: false    })    blobStream.on('finish', () => {        const path = format(          `${blob.name}`        )        resolve({path})      })      .on('error', () => {        reject(`Unable to upload image, something went wrong`)      })      .end(file.buffer)  })}export async function deleteFile(path: string): Promise<void> {  try {    const blob = bucket.file(path);    await blob.delete();  } catch (error) {    console.log(error);  }}
+import {Storage} from "@google-cloud/storage";
+import {Folder} from "../../types/Folder";
+import {Express} from "express";
+import {format} from "util";
+
+
+const storage = new Storage({
+  projectId: process.env.GCLOUD_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GCLOUD_CLIENT_EMAIL,
+    private_key: process.env.GCLOUD_PRIVATE_KEY.replace(/\\n/g, '\n')
+  }
+});
+const bucket = storage.bucket(process.env.GCLOUD_BUCKET_NAME);
+
+// get temporary link for file download from google cloud storage bucket (expires in 5 minutes)
+export async function getTemporaryLink(path: string, expires: number = 300000) {
+  const blob = bucket.file(path);
+  const data = await blob.getSignedUrl({
+    action: 'read',
+    expires: new Date(Date.now() + expires)
+  });
+  return data[0];
+}
+
+export async function upload(folder: Folder, name: string, file: Express.Multer.File): Promise<{ path: string }> {
+  return new Promise((resolve, reject) => {
+
+    const blob = bucket.file(`${folder}/${name.replace(/ /g, "_")}`)
+    const blobStream = blob.createWriteStream({
+      resumable: false
+    })
+    blobStream.on('finish', () => {
+        const path = format(
+          `${blob.name}`
+        )
+        resolve({path})
+      })
+      .on('error', () => {
+        reject(`Unable to upload image, something went wrong`)
+      })
+      .end(file.buffer)
+  })
+}
+
+export async function deleteFile(path: string): Promise<void> {
+  try {
+    const blob = bucket.file(path);
+    await blob.delete();
+  } catch (error) {
+    console.log(error);
+  }
+}
--- src/routes/timetables.routes.ts
+++ src/routes/timetables.routes.ts
@@ -1,1 +1,147 @@
-import express, {Request, Response} from 'express';import {User} from "../models/users.model";import {formattedResponse} from "../handlers/http/formattedResponse";import {TimetablesController} from "../controllers/Timetables.controller";import {check, validationResult} from "express-validator";import {validationMessage} from "../handlers/http/validationMessage";import {objectParser} from "../handlers/objects/objectParser";const timetablesRouter = express.Router({mergeParams: true});/* GET timetables/:id - get timetable by id */timetablesRouter.get('/:id', async (req: Request, res: Response) => {  const {params: {id}} = req;  try {    const controller = new TimetablesController();    const user: User = req.body.user    const response = await controller.getById(id);    if (Object.keys(response).length === 0) {      return res.status(404).json(        formattedResponse({          status: 404,          object: "timetable",        })      )    }    return res.json(response);  } catch (error) {    res.status(500).json(      formattedResponse({        status: 500,        Error: error,        object: "timetable",      })    )  }});/* POST timetables/ - upload new timetable */timetablesRouter.post('/',  [    check("object.companyId")      .not().isEmpty()      .withMessage('Company id id cannot be empty'),    check("object.refId")      .not().isEmpty()      .withMessage('Ref id cannot be empty'),    check("object.moduleId")      .not().isEmpty()      .withMessage('Module id cannot be empty')  ],  async (req: Request, res: Response) => {    if (!validationResult(req).isEmpty()) {      return res.status(400).json(        formattedResponse({          status: 400,          object: "timetable",          message: validationMessage(req)        })      );    }    try {      const user: User = req.body.user      const controller = new TimetablesController();      const response = await controller.create(objectParser(req.body.object));      res.status(200).json(response);    } catch (error) {      res.status(500).send(        formattedResponse({          status: 500,          Error: error,          object: "timetable",        })      );    }  });/* PUT timetables/:id - update existing timetable */timetablesRouter.put('/:id', async (req: Request, res: Response) => {  const {    params: {id},  } = req;  if (!id) return;  try {    const user: User = req.body.user    const controller = new TimetablesController();    const response = await controller.update(id, objectParser(req.body.object));    res.status(200).json(response);  } catch (error) {    res.status(500).json(      formattedResponse({        status: 500,        Error: error,        object: "timetable",      })    )  }});/* DELETE timetables/:id - logic delete timetable */timetablesRouter.delete('/:id', async (req: Request, res: Response) => {  const {    params: {id},  } = req;  if (!id) return;  try {    const user: User = req.body.user    const controller = new TimetablesController();    const response = await controller.logicDelete(id);    res.status(200).json(response);  } catch (error) {    res.status(500).json(formattedResponse({      status: 500,      object: "timetable",    }));  }});timetablesRouter.get('/', async (req: Request, res: Response) => {  const {    query: {moduleId, refId},  } = req;  if (!moduleId || !refId) return;  try {    const user: User = req.body.user    const controller = new TimetablesController();    const response = await controller.getByModule(moduleId as string, refId as string);    res.status(200).json(response);  } catch (error) {    res.status(500).json(      formattedResponse({        status: 500,        object: "timetable",      }));  }});export default timetablesRouter;
+import express, {Request, Response} from 'express';
+import {User} from "../models/users.model";
+import {formattedResponse} from "../handlers/http/formattedResponse";
+import {TimetablesController} from "../controllers/Timetables.controller";
+import {check, validationResult} from "express-validator";
+import {validationMessage} from "../handlers/http/validationMessage";
+import {objectParser} from "../handlers/objects/objectParser";
+
+const timetablesRouter = express.Router({mergeParams: true});
+
+/* GET timetables/:id - get timetable by id */
+timetablesRouter.get('/:id', async (req: Request, res: Response) => {
+
+  const {params: {id}} = req;
+
+  try {
+    const controller = new TimetablesController();
+    const user: User = req.body.user
+    const response = await controller.getById(id);
+    if (Object.keys(response).length === 0) {
+      return res.status(404).json(
+        formattedResponse({
+          status: 404,
+          object: "timetable",
+        })
+      )
+    }
+    return res.json(response);
+  } catch (error) {
+    res.status(500).json(
+      formattedResponse({
+        status: 500,
+        Error: error,
+        object: "timetable",
+      })
+    )
+  }
+});
+
+/* POST timetables/ - upload new timetable */
+timetablesRouter.post('/',
+  [
+    check("object.companyId")
+      .not().isEmpty()
+      .withMessage('Company id id cannot be empty'),
+    check("object.refId")
+      .not().isEmpty()
+      .withMessage('Ref id cannot be empty'),
+    check("object.moduleId")
+      .not().isEmpty()
+      .withMessage('Module id cannot be empty')
+  ],
+  async (req: Request, res: Response) => {
+
+    if (!validationResult(req).isEmpty()) {
+      return res.status(400).json(
+        formattedResponse({
+          status: 400,
+          object: "timetable",
+          message: validationMessage(req)
+        })
+      );
+    }
+
+    try {
+      const user: User = req.body.user
+      const controller = new TimetablesController();
+      const response = await controller.create(objectParser(req.body.object));
+      res.status(200).json(response);
+    } catch (error) {
+      res.status(500).send(
+        formattedResponse({
+          status: 500,
+          Error: error,
+          object: "timetable",
+        })
+      );
+    }
+
+  });
+
+/* PUT timetables/:id - update existing timetable */
+timetablesRouter.put('/:id', async (req: Request, res: Response) => {
+
+  const {
+    params: {id},
+  } = req;
+  if (!id) return;
+
+  try {
+    const user: User = req.body.user
+    const controller = new TimetablesController();
+    const response = await controller.update(id, objectParser(req.body.object));
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(
+      formattedResponse({
+        status: 500,
+        Error: error,
+        object: "timetable",
+      })
+    )
+  }
+});
+
+/* DELETE timetables/:id - logic delete timetable */
+timetablesRouter.delete('/:id', async (req: Request, res: Response) => {
+  const {
+    params: {id},
+  } = req;
+  if (!id) return;
+
+  try {
+    const user: User = req.body.user
+    const controller = new TimetablesController();
+    const response = await controller.logicDelete(id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(formattedResponse({
+      status: 500,
+      object: "timetable",
+    }));
+  }
+});
+
+
+timetablesRouter.get('/', async (req: Request, res: Response) => {
+  const {
+    query: {moduleId, refId},
+  } = req;
+  if (!moduleId || !refId) return;
+
+  try {
+    const user: User = req.body.user
+    const controller = new TimetablesController();
+    const response = await controller.getByModule(moduleId as string, refId as string);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(500).json(
+      formattedResponse({
+        status: 500,
+        object: "timetable",
+      }));
+  }
+});
+
+export default timetablesRouter;
